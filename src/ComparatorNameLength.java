8f9eafbf2356a1cd69bf52e9c897a0f6fd16c5dc
cloud866 <dklein927@gmail.com>
2023-03-01 19:08:04 +0300

    Initial commit

---
+++ src/ComparatorNameLength.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class ComparatorNameLength implements Comparator<Pet> {
+
+    @Override
+    public int compare(Pet o1, Pet o2) {
+        return o2.getName().length() - o1.getName().length();
+    }
+}
